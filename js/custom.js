5bb4acb744a69649da7916f38263df18d7fc44e5
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-03-01 16:17:57 +0800

    set gellery part

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
   $(".carousel").carousel();
 });
+
+function changeBg(bg, title) {
+  const banner = document.querySelector("#content-bg");
+  const contents = document.querySelectorAll(".content");
+  banner.style.background = `url("../images/background/${bg}")`;
+  banner.style.backgroundSize = "cover";
+  banner.style.backgroundPosition = "center";
+
+  contents.forEach((content) => {
+    content.classList.remove("active");
+    if (content.classList.contains(title)) {
+      content.classList.add("active");
+    }
+  });
+}
+
